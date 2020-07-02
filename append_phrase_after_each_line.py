73783c585eaa5209ea4cc6b0da88cb8dd5281f60
Luiz C <luizdantas.cavalcanti@gmail.com>
2020-07-02 02:08:37 -0300

    Just ran autopep8 in the script

--- append_phrase_after_each_line.py
+++ append_phrase_after_each_line.py
@@ -4,15 +4,17 @@
     Apply some treatment for a dns server ip list, so that the list could be
     parsed by a particular dns benchmarking application.
     """
-    with open(file,"r") as source:
+    with open(file, "r") as source:
         slines = source.readlines()
-    with open(file,"w") as ap:
+    with open(file, "w") as ap:
         for ip in slines:
             ip = ip[:-1]
             ap.write(ip)
-            for x in range(0, (30 - len(ip)) ):
+            for x in range(0, 30 - len(ip)):
                 ap.write(" ")
             ap.write(prase)
+        ap.write("\n")
+
 
 if __name__ == "__main__":
     append_DNS_list("a.txt")
