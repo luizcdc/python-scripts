1684ecc554a706cd73f39050ba905cbad5ac56c8
Luiz C <luizdantas.cavalcanti@gmail.com>
2020-07-02 01:21:47 -0300

    added a very specific script not useful for anybody haha

---
+++ append_phrase_after_each_line.py
@@ -0,0 +1,18 @@
+﻿def append_DNS_list(file,
+                    prase="··· no official Internet DNS name ···\n"):
+    """
+    Apply some treatment for a dns server ip list, so that the list could be
+    parsed by a particular dns benchmarking application.
+    """
+    with open(file,"r") as source:
+        slines = source.readlines()
+    with open(file,"w") as ap:
+        for ip in slines:
+            ip = ip[:-1]
+            ap.write(ip)
+            for x in range(0, (30 - len(ip)) ):
+                ap.write(" ")
+            ap.write(prase)
+
+if __name__ == "__main__":
+    append_DNS_list("a.txt")
